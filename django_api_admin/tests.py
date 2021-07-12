1d9c555c8f31a9d63cea729c2172e93194269f89
mohammad salah ali <MsMainAcc0unt@gmail.com>
2021-07-12 11:12:27 +0200

    create LogoutView, test LogoutView.

--- django_api_admin/tests.py
+++ django_api_admin/tests.py
@@ -1,16 +1,16 @@
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework.test import APITestCase, URLPatternsTestCase
-from django_api_admin.views import LoginView
+from django_api_admin.views import LoginView, LogoutView
 from django.urls import path
-from django.contrib.auth.models import UserManager
 
 UserModel = get_user_model()
 
 
 class AuthenticationTestCase(APITestCase, URLPatternsTestCase):
     urlpatterns = [
-        path('apiadmin/login', LoginView.as_view(), name='login')
+        path('apiadmin/login', LoginView.as_view(), name='login'),
+        path('apiadmin/logout', LogoutView.as_view(), name='logout')
     ]
 
     def test_non_staff_user_login(self):
@@ -30,3 +30,25 @@ class AuthenticationTestCase(APITestCase, URLPatternsTestCase):
         url = reverse('login')
         response = self.client.post(url, {'username': user.username, 'password': 'password'})
         self.assertEqual(response.status_code, 200)
+
+    def test_logout_user_logged_in(self):
+        user = UserModel.objects.create_superuser(username='admin')
+        user.set_password('password')
+        user.save()
+
+        self.client.force_login(user=user)
+        url = reverse('logout')
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.data.get('message', None))
+
+    def test_logout_user_not_logged_in(self):
+        user = UserModel.objects.create_superuser(username='admin')
+        user.set_password('password')
+        user.save()
+
+        url = reverse('logout')
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.data.get('message', None))
+
--- django_api_admin/views.py
+++ django_api_admin/views.py
@@ -2,7 +2,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django_api_admin.serializers import LoginSerializer, UserSerializer
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
+from django.utils.translation import gettext_lazy as _
 
 
 class LoginView(APIView):
@@ -22,3 +23,14 @@ class LoginView(APIView):
                 return Response(serializer.errors, status=status.HTTP_403_FORBIDDEN)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutView(APIView):
+    def get(self, request):
+        user_serializer = UserSerializer(request.user)
+        message = _("You are logged out.")
+        logout(request)
+        return Response({'user': user_serializer.data, 'message': message}, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        return self.get(request)
